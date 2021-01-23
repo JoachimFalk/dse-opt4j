a7e1e4d82631624b633ed0a1a2a7956c3c321c65
Joachim Falk <joachim.falk@fau.de>
2021-01-23 11:17:13 +0100

    Fixed javadoc

--- opt4j/opt4j-core/src/main/java/org/opt4j/core/common/random/MTRandom.java
+++ opt4j/opt4j-core/src/main/java/org/opt4j/core/common/random/MTRandom.java
@@ -53,7 +53,7 @@ package org.opt4j.core.common.random;
  *         <!-- Creative Commons License --> <a
  *         href="http://creativecommons.org/licenses/LGPL/2.1/"><img
  *         alt="CC-GNU LGPL" border="0"
- *         src="http://creativecommons.org/images/public/cc-LGPL-a.png" /></a><br />
+ *         src="http://creativecommons.org/images/public/cc-LGPL-a.png"></a><br>
  *         This software is licensed under the <a
  *         href="http://creativecommons.org/licenses/LGPL/2.1/">CC-GNU LGPL</a>.
  *         <!-- /Creative Commons License -->
